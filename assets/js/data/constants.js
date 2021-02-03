171923d0d853022b0437b8ed7c3c8ab29678c3ff
Mikhael6363 <mikhael.6363@gmail.com>
2021-02-04 00:27:51 +0200

    Add data

---
+++ assets/js/data/constants.js
@@ -0,0 +1,7 @@
+const USER_DESCRIPTION =
+  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur voluptatum omnis similique? Fugiat, ipsam nisi. Delectus consequatur quis perspiciatis in error ducimus libero, iste inventore iustonatus, aspernatur quibusdam praesentium? Distinctio ratione dolorem assumenda tempora libero odit molestiae deserunt illo voluptatem eius autem explicabo eligendi totam harum culpa, cum necessitatibus.';
+
+const SOCIAL_LINKS_MAP = new Map()
+  .set('www.facebook.com', 'fa-facebook-f')
+  .set('twitter.com', 'fa-twitter')
+  .set('www.instagram.com', 'fa-instagram');
