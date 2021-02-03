f9a2551042464e2b55f95292b26eed6d234849aa
Mikhael6363 <mikhael.6363@gmail.com>
2021-02-04 00:31:58 +0200

    Add image event handlers (load & error)

---
+++ assets/js/handlers/imageLoadError.js
@@ -0,0 +1,12 @@
+function handleImageError({ target }) {
+  target.remove();
+}
+
+function handleImageLoad({
+  target,
+  target: {
+    dataset: { id },
+  },
+}) {
+  document.getElementById(`wrapper${id}`).append(target);
+}
